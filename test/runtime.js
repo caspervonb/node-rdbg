18888ee209ac1c7aad7a1fceb407a366f6469520
janstovr <janstovr@qup.io>
2016-01-12 02:19:00 +0100

    test: add coverage for callFunctionOn
    There was no coverage for callFunctionOn method in runtime.
    
    This adds coverage for the named method.

---
+++ test/runtime.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const rdbg = require('..');
+const test = require('tape');
+const ws = require('ws');
+
+test('Runtime.callFunctionOn', assert => {
+
+  let server = ws.createServer({port: '4000'});
+  server.once('connection', connection => {
+
+  	connection.on('message', data => {
+      let message = JSON.parse(data);
+  		connection.send(JSON.stringify({
+  			id: message.id,
+        error: false,
+        result: {
+          result: {type: 'object'}
+        }
+  		}));
+  	});
+  });
+
+  server.on('listening', () => {
+
+    let client = rdbg.connect('ws://localhost:4000');
+    client.once('connect', () => {
+      let runtime = client.runtime;
+
+      client.once('response', (request, response) => {
+        assert.deepEqual({functionDeclaration: '()=>{}', objectId: 1}, request.params, 'request to call a declared function on the given object');
+        assert.deepEqual({type: 'object'}, response.result.result, 'response with result from the call');
+
+
+        runtime.callFunctionOn(1, '()=>{}', {arguments: [{ value: '5' }]}, (error, result) => {});
+        client.once('response', (request, response) => {
+          assert.deepEqual({functionDeclaration: '()=>{}', arguments: [{ value: '5' }], objectId: 1 }, request.params, 'request to call a declared function on the given object with arguments');
+          assert.deepEqual({type: 'object'}, response.result.result, 'response with result from the call');
+          server.close();
+          assert.end();
+        });
+      });
+
+      runtime.callFunctionOn(1, '()=>{}', (error, result) => {});
+    });
+  });
+});

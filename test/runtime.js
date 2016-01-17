ec731fdfdb036d35a29289e470a56a17cd532c5b
janstovr <janstovr@qup.io>
2016-01-18 00:49:38 +0100

    test: add runtime enable test

--- test/runtime.js
+++ test/runtime.js
@@ -48,3 +48,37 @@ test('runtime call', assert => {
     });
   });
 });
+
+test('runtime enable', assert => {
+  assert.plan(3);
+  let server = ws.createServer({port: '4000'});
+
+  server.once('connection', connection => {
+
+    connection.once('close', () => {
+      assert.pass('close');
+    });
+
+    connection.on('message', data => {
+      let message = JSON.parse(data);
+      connection.send(JSON.stringify({
+        id: message.id
+      }));
+    });
+  });
+
+  server.on('listening', () => {
+    let client = rdbg.connect('ws://localhost:4000');
+    client.once('connect', () => {
+
+      client.once('response', (request, response) => {
+        assert.equals('Runtime.enable', request.method);
+      });
+
+      client.runtime.enable(error => {
+        assert.error(error);
+        server.close();
+      });
+    });
+  });
+});

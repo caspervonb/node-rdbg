c07169ab840f935106cbe02bd9512d432fa6fe8d
janstovr <janstovr@qup.io>
2016-01-18 03:03:06 +0100

    test: add debugger pause test

--- test/debugger.js
+++ test/debugger.js
@@ -71,3 +71,37 @@ test('debugger disable', assert => {
     });
   });
 });
+
+test('debugger pause', assert => {
+  assert.plan(3);
+  let server = ws.createServer({port: '4000'});
+
+  server.once('connection', connection => {
+
+    connection.once('close', () => {
+      assert.pass('close');
+    });
+
+    connection.on('message', data => {
+      let message = JSON.parse(data);
+      connection.send(JSON.stringify({
+        id: message.id
+      }));
+    });
+  });
+
+  server.on('listening', () => {
+    let client = rdbg.connect('ws://localhost:4000');
+    client.once('connect', () => {
+
+      client.once('response', (request, response) => {
+        assert.equals('Debugger.pause', request.method);
+      });
+
+      client.debugger.pause(error => {
+        assert.error(error);
+        server.close();
+      });
+    });
+  });
+});
